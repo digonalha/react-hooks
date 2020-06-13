aa74fa594224eeac5125bd51c7389ce0bc185060
Rodrigo Fernandes <digonalha@gmail.com>
2020-06-13 00:08:17 -0300

    implementando useState, useEffect, useMemo e useCallback

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
   const [techs, setTech] = useState(['ReactJS', 'ReactNative']);
   const [newTech, setNewTech] = useState('');
 
-  function handleAdd() {
+  const handleAdd = useCallback(() => {
     setTech([...techs, newTech]);
     setNewTech('');
-  }
+  }, [newTech, techs]);
+
+  // DidMount: executa somente da primeira vez que o componente é carregado em tela
+  // Quando não passamos nada para o useEffect monitorar (segundo parametro da função),
+  // ele só é executado quando o componente é renderizado
+  useEffect(() => {
+    const listTech = localStorage.getItem('techs');
+
+    if (listTech) setTech(JSON.parse(listTech));
+
+    // para utilizar como um didWillUnmount, é necessario ao final da função implementar a função do que irá ser feito ao deixar de carregar:
+    // return() => {faz qualquer coisa};
+  }, []);
+
+  // DidUpdate: monitorar alterações na lista de tecnologias =>
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(techs));
+  }, [techs]);
+
+  // só será executada quando a variavel techs tiver seu valor alterado
+  const techSize = useMemo(() => techs.length, [techs]);
+
   return (
     <>
       <ul>
@@ -15,6 +36,8 @@ function App() {
           <li key={tech}>{tech}</li>
         ))}
       </ul>
+      <strong>VocÊ tem {techSize} tecnologias.</strong>
+      <br />
       <input value={newTech} onChange={(e) => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>
         Adicionar
